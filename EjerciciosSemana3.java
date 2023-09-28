681eb28e87f64d75f539b83cbecc8a8575ff2770
danielaguilar14 <145624262+danielaguilar14@users.noreply.github.com>
2023-09-27 20:24:28 -0600

    Add files via upload

---
+++ EjerciciosSemana3.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciossemana3;
+
+/**
+ *
+ * @author dagui
+ */
+public class EjerciciosSemana3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Ejercicio #1
+         for (int i = 0; i < 5; i++) {
+            // Imprimimos 10 símbolos "@" en cada línea
+            for (int j = 0; j < 10; j++) {
+                System.out.print("@");
+            }
+            // Imprimimos un salto de línea para pasar a la siguiente línea
+            System.out.println();
+        }
+    }
+    }
+     
